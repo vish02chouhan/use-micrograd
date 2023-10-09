905c246de3d6ff73b34845b21f7382c972862ec0
Vishwajeet Singh <vishwajeet.chouhan@qpharmacorp.com>
2023-10-08 20:26:50 -0400

    add to git

---
+++ make-more.py
@@ -0,0 +1,186 @@
+import torch
+import torch.nn.functional as F
+from torch.utils.tensorboard import SummaryWriter
+
+words = open('names.txt', 'r').read().splitlines()
+
+chars = sorted(list(set(''.join(words))))
+stoi = {s:i+1 for i,s in enumerate(chars)}
+stoi['.'] = 0
+itos = {i:s for s,i in stoi.items()}
+
+
+# # create the training set of bigrams (x,y)
+# xs, ys = [], []
+
+# for w in words[:1]:
+#   chs = ['.'] + list(w) + ['.']
+#   for ch1, ch2 in zip(chs, chs[1:]):
+#     ix1 = stoi[ch1]
+#     ix2 = stoi[ch2]
+#     print(ch1, ch2)
+#     xs.append(ix1)
+#     ys.append(ix2)
+    
+# xs = torch.tensor(xs)
+# ys = torch.tensor(ys)
+
+# import torch.nn.functional as F
+# xenc = F.one_hot(xs, num_classes=27).float()
+
+# # randomly initialize 27 neurons' weights. each neuron receives 27 inputs
+# g = torch.Generator().manual_seed(2147483647)
+# W = torch.randn((27, 27), generator=g)
+
+# xenc = F.one_hot(xs, num_classes=27).float() # input to the network: one-hot encoding
+# logits = xenc @ W # predict log-counts
+# counts = logits.exp() # counts, equivalent to N
+# probs = counts / counts.sum(1, keepdims=True) # probabilities for next character
+# # btw: the last 2 lines here are together called a 'softmax'
+
+# nlls = torch.zeros(5)
+# for i in range(5):
+#   # i-th bigram:
+#   x = xs[i].item() # input character index
+#   y = ys[i].item() # label character index
+#   print('--------')
+#   print(f'bigram example {i+1}: {itos[x]}{itos[y]} (indexes {x},{y})')
+#   print('input to the neural net:', x)
+#   print('output probabilities from the neural net:', probs[i])
+#   print('label (actual next character):', y)
+#   p = probs[i, y]
+#   print('probability assigned by the net to the the correct character:', p.item())
+#   logp = torch.log(p)
+#   print('log likelihood:', logp.item())
+#   nll = -logp
+#   print('negative log likelihood:', nll.item())
+#   nlls[i] = nll
+
+# print('=========')
+# print('average negative log likelihood, i.e. loss =', nlls.mean().item())
+
+
+# create the dataset
+xs, ys = [], []
+# for w in words[:1][0:1]:
+for w in ["emma","emma","emma","emma","emma","emma","emma","emma","emma","emma"]:
+  chs = ['.'] + list(w) + ['.']
+  for ch1, ch2 in zip(chs, chs[1:]):
+    ix1 = stoi[ch1]
+    ix2 = stoi[ch2]
+    xs.append(ix1)
+    ys.append(ix2)
+xs = torch.tensor(xs)
+ys = torch.tensor(ys)
+num = xs.nelement()
+print('number of examples: ', num)
+
+# initialize the 'network'
+g = torch.Generator().manual_seed(2147483647)
+W = torch.randn((27, 27), generator=g, requires_grad=True)
+
+
+# gradient descent
+for k in range(1):
+  
+  # forward pass
+  xenc = F.one_hot(xs, num_classes=27).float() # input to the network: one-hot encoding
+  
+  def get_logits(direct: False):
+      if direct:
+          import numpy as np
+          logits_array = []
+          for index, i_c in enumerate(xenc):
+            #i_c = [1,0,0....] for ,
+            my_array = np.array(i_c.item()) 
+            index_value = np.where(my_array == 1)
+
+            logits_array.append(W[index, index_value])
+          
+          return logits_array
+
+
+      else:
+        return xenc @ W
+
+  logits = xenc @ W
+
+  print(logits)
+  print(W[0])
+
+  counts = logits.exp() # counts, equivalent to N
+  print(counts)
+  probs = counts / counts.sum(1, keepdims=True) # probabilities for next character
+  print(probs)
+
+  # print('Probability of e:',probs[[0], [5]])
+  # print('Probability of e:',probs[[0], [5]].log())
+  # #print('Probability of e:',probs[[0,1,2,3...268000], [5,1,3,15,26,22...]].log())
+  # print('Probability of e:',probs[[0], [5]].log().mean() + 0.05*(W**2).mean())
+
+  loss = -probs[torch.arange(num), ys].log().mean() + 0.05*(W**2).mean()
+
+  #2.56
+  #print(loss.item())
+  
+  # backward pass
+  W.grad = None # set to zero the gradient
+  loss.backward()
+
+  # update
+  W.data += -1 * W.grad
+ 
+
+
+  # finally, sample from the 'neural net' model
+g = torch.Generator().manual_seed(2147483647)
+
+for i in range(5):
+  
+  out = []
+  ix = 0
+  while True:
+    
+    # ----------
+    # BEFORE:
+    #p = P[ix]
+    # ----------
+    # NOW:
+    xenc = F.one_hot(torch.tensor([ix]), num_classes=27).float()
+
+    def get_logits(direct: False):
+      if direct:
+          import numpy as np
+          logits_array = []
+          actual_logits = xenc @ W
+          for index, i_c in enumerate(xenc):
+            #i_c = [1,0,0....] for ,
+            my_array = np.array(i_c.item()) 
+            index_value = np.where(my_array == 1)
+
+            logits_array.append(W[index, index_value])
+          
+         
+          return logits_array
+
+
+      else:
+        return xenc @ W
+
+    logits = get_logits(True)
+    
+    #logits = xenc @ W # predict log-counts
+    
+    if ix == 0:
+      logits[0,2] = 50
+    counts = logits.exp() # counts, equivalent to N
+    p = counts / counts.sum(1, keepdims=True) # probabilities for next character
+    # ----------
+    
+    ix = torch.multinomial(p, num_samples=1, replacement=True, generator=g).item()
+    out.append(itos[ix])
+    if len(out) > 5:
+      break
+    if ix == 0:
+      break
+  print(''.join(out))
---
+++ train-dummy.py
@@ -0,0 +1,204 @@
+import math
+import random
+
+logits = .1
+count_logits = math.exp(logits)
+stop = 1
+
+class Value:
+  
+  def __init__(self, data, _children=(), _op='', label=''):
+    self.data = data
+    self.grad = 0.0
+    self._backward = lambda: None
+    self._prev = set(_children)
+    self._op = _op
+    self.label = label
+  
+  def __repr__(self):
+    return f"Value(data={self.data})"
+  
+  def __add__(self, other):
+    other = other if isinstance(other, Value) else Value(other)
+    out = Value(self.data + other.data, (self, other), '+')
+    
+    def _backward():
+      self.grad += 1.0 * out.grad
+      other.grad += 1.0 * out.grad
+    out._backward = _backward
+    
+    return out
+  
+  def __mul__(self, other):
+    other = other if isinstance(other, Value) else Value(other)
+    out = Value(self.data * other.data, (self, other), '*')
+    
+    def _backward():
+      self.grad += other.data * out.grad
+      other.grad += self.data * out.grad
+    out._backward = _backward
+      
+    return out
+  
+  def __pow__(self, other):
+    assert isinstance(other, (int, float)), "only supporting int/float powers for now"
+    out = Value(self.data**other, (self,), f'**{other}')
+    
+    def _backward():
+      self.grad += other * (self.data ** (other - 1)) * out.grad
+      
+    out._backward = _backward
+    return out
+  
+  def __rmul__(self, other): # other * self
+    return self * other
+  
+  def __truediv__(self, other): # self / other
+    return self * other**-1
+  
+  def __neg__(self): # -self
+    return self * -1
+  
+  def __sub__(self, other): # self - other
+    return self + (-other)
+  
+  def __radd__(self, other): # other + self
+    return self + other
+  
+  def tanh(self):
+    x = self.data
+    t = (math.exp(2*x) - 1)/(math.exp(2*x) + 1)
+    out = Value(t, (self, ), 'tanh')
+    
+    def _backward():
+      self.grad += (1 - t**2) * out.grad
+    out._backward = _backward
+    
+    return out
+  
+  def exp(self):
+    x = self.data
+    out = Value(math.exp(x), (self, ), 'exp')
+    
+    def _backward():
+      self.grad += out.data * out.grad # NOTE: in the video I incorrectly used = instead of +=. Fixed here.
+    out._backward = _backward
+    
+    return out
+  
+  
+  def backward(self):
+    
+    topo = []
+    visited = set()
+    def build_topo(v):
+      if v not in visited:
+        visited.add(v)
+        for child in v._prev:
+          build_topo(child)
+        topo.append(v)
+    build_topo(self)
+    
+    self.grad = 1.0
+    for node in reversed(topo):
+      node._backward()
+
+
+class Neuron:
+  
+  def __init__(self, nin):
+    self.w = [Value(random.uniform(-1,1)) for _ in range(nin)]
+    self.b = Value(random.uniform(-1,1))
+  
+  def __call__(self, x):
+    # w * x + b
+    act = sum((wi*xi for wi, xi in zip(self.w, x)), self.b)
+    out = act.tanh()
+    return out
+  
+  def parameters(self):
+    return self.w + [self.b]
+
+class Layer:
+  
+  def __init__(self, nin, nout):
+    self.neurons = [Neuron(nin) for _ in range(nout)]
+  
+  def __call__(self, x):
+    outs = [n(x) for n in self.neurons]
+    return outs[0] if len(outs) == 1 else outs
+  
+  def parameters(self):
+    return [p for neuron in self.neurons for p in neuron.parameters()]
+
+class MLP:
+  
+  def __init__(self, nin, nouts):
+    sz = [nin] + nouts
+    self.layers = [Layer(sz[i], sz[i+1]) for i in range(len(nouts))]
+  
+  def __call__(self, x):
+    for layer in self.layers:
+      x = layer(x)
+    return x
+  
+  def parameters(self):
+    return [p for layer in self.layers for p in layer.parameters()]
+
+
+
+n = MLP(3, [2, 1])
+
+xs = [
+  [2.0, 3.0, -1.0],
+  # [3.0, -1.0, 0.5],
+  # [0.5, 1.0, 1.0],
+  # [1.0, 1.0, -1.0],
+]
+ys = [1.0, -1.0, -1.0, 1.0] # desired targets
+
+for k in range(20):
+  
+  # forward pass
+  ypred = [n(x) for x in xs]
+  loss = sum((yout - ygt)**2 for ygt, yout in zip(ys, ypred))
+  
+  # backward pass
+  for p in n.parameters():
+    p.grad = 0.0
+  loss.backward()
+  
+  # update
+  for p in n.parameters():
+    p.data += -0.1 * p.grad
+  
+  print(k, loss.data)
+  
+# ​
+
+# n = MLP(3, [4, 4, 1])
+# xs = [
+#   [2.0, 3.0, -1.0],
+#   [3.0, -1.0, 0.5],
+#   [0.5, 1.0, 1.0],
+#   [1.0, 1.0, -1.0],
+# ]
+# ys = [1.0, -1.0, -1.0, 1.0] # desired targets
+
+# for k in range(20):
+  
+#   # forward pass
+#   ypred = [n(x) for x in xs]
+#  # print(ypred)
+#   loss = sum((yout - ygt)**2 for ygt, yout in zip(ys, ypred))
+  
+#   # backward pass
+#   for p in n.parameters():
+#     p.grad = 0.0
+#   loss.backward()
+  
+#   # update
+#   for p in n.parameters():
+#     p.data += -0.1 * p.grad
+  
+#   print(k, loss.data)
