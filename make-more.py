899a1308a2eaff6ba2ec35cd4eefb514fab1509c
Vishwajeet Singh <vishwajeet.chouhan@qpharmacorp.com>
2023-10-16 18:08:46 -0400

    added new code

--- make-more.py
+++ make-more.py
@@ -10,51 +10,51 @@ stoi['.'] = 0
 itos = {i:s for s,i in stoi.items()}
 
 
-# # create the training set of bigrams (x,y)
-# xs, ys = [], []
+# create the training set of bigrams (x,y)
+xs, ys = [], []
 
-# for w in words[:1]:
-#   chs = ['.'] + list(w) + ['.']
-#   for ch1, ch2 in zip(chs, chs[1:]):
-#     ix1 = stoi[ch1]
-#     ix2 = stoi[ch2]
-#     print(ch1, ch2)
-#     xs.append(ix1)
-#     ys.append(ix2)
+for w in words:
+  chs = ['.'] + list(w) + ['.']
+  for ch1, ch2 in zip(chs, chs[1:]):
+    ix1 = stoi[ch1]
+    ix2 = stoi[ch2]
+    print(ch1, ch2)
+    xs.append(ix1)
+    ys.append(ix2)
     
-# xs = torch.tensor(xs)
-# ys = torch.tensor(ys)
+xs = torch.tensor(xs)
+ys = torch.tensor(ys)
 
-# import torch.nn.functional as F
-# xenc = F.one_hot(xs, num_classes=27).float()
+import torch.nn.functional as F
+xenc = F.one_hot(xs, num_classes=27).float()
 
-# # randomly initialize 27 neurons' weights. each neuron receives 27 inputs
-# g = torch.Generator().manual_seed(2147483647)
-# W = torch.randn((27, 27), generator=g)
+# randomly initialize 27 neurons' weights. each neuron receives 27 inputs
+g = torch.Generator().manual_seed(2147483647)
+W = torch.randn((27, 27), generator=g)
 
-# xenc = F.one_hot(xs, num_classes=27).float() # input to the network: one-hot encoding
-# logits = xenc @ W # predict log-counts
-# counts = logits.exp() # counts, equivalent to N
-# probs = counts / counts.sum(1, keepdims=True) # probabilities for next character
-# # btw: the last 2 lines here are together called a 'softmax'
-
-# nlls = torch.zeros(5)
-# for i in range(5):
-#   # i-th bigram:
-#   x = xs[i].item() # input character index
-#   y = ys[i].item() # label character index
-#   print('--------')
-#   print(f'bigram example {i+1}: {itos[x]}{itos[y]} (indexes {x},{y})')
-#   print('input to the neural net:', x)
-#   print('output probabilities from the neural net:', probs[i])
-#   print('label (actual next character):', y)
-#   p = probs[i, y]
-#   print('probability assigned by the net to the the correct character:', p.item())
-#   logp = torch.log(p)
-#   print('log likelihood:', logp.item())
-#   nll = -logp
-#   print('negative log likelihood:', nll.item())
-#   nlls[i] = nll
+xenc = F.one_hot(xs, num_classes=27).float() # input to the network: one-hot encoding
+logits = xenc @ W # predict log-counts
+counts = logits.exp() # counts, equivalent to N
+probs = counts / counts.sum(1, keepdims=True) # probabilities for next character
+# btw: the last 2 lines here are together called a 'softmax'
+print(probs)
+nlls = torch.zeros(5)
+for i in range(5):
+  # i-th bigram:
+  x = xs[i].item() # input character index
+  y = ys[i].item() # label character index
+  print('--------')
+  print(f'bigram example {i+1}: {itos[x]}{itos[y]} (indexes {x},{y})')
+  print('input to the neural net:', x)
+  print('output probabilities from the neural net:', probs[i])
+  print('label (actual next character):', y)
+  p = probs[i, y]
+  print('probability assigned by the net to the the correct character:', p.item())
+  logp = torch.log(p)
+  print('log likelihood:', logp.item())
+  nll = -logp
+  print('negative log likelihood:', nll.item())
+  nlls[i] = nll
 
 # print('=========')
 # print('average negative log likelihood, i.e. loss =', nlls.mean().item())
@@ -118,8 +118,10 @@ for k in range(1):
   # #print('Probability of e:',probs[[0,1,2,3...268000], [5,1,3,15,26,22...]].log())
   # print('Probability of e:',probs[[0], [5]].log().mean() + 0.05*(W**2).mean())
 
-  loss = -probs[torch.arange(num), ys].log().mean() + 0.05*(W**2).mean()
 
+
+  loss = -probs[torch.arange(num), ys].log().mean() + 0.05*(W**2).mean()
+  #probs[torch.arange(num), ys] size is 268000
   #2.56
   #print(loss.item())
   
@@ -133,6 +135,7 @@ for k in range(1):
 
 
   # finally, sample from the 'neural net' model
+
 g = torch.Generator().manual_seed(2147483647)
 
 for i in range(5):
